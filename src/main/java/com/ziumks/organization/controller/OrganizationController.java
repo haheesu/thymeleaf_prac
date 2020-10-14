b04bed0c77aea426f9e720e43c2b20cfba288b4c
haheesu <hhs@ziumks.com>
2020-10-14 16:23:49 +0900

    first

--- src/main/java/com/ziumks/organization/controller/OrganizationController.java
+++ src/main/java/com/ziumks/organization/controller/OrganizationController.java
@@ -1,9 +1,8 @@
 package com.ziumks.organization.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +22,9 @@ public class OrganizationController {
 	
 	@GetMapping("{id}")
 	Organization getOrganization(@PathVariable Long id) {
-	    return organizationMapper.findById(id);
+		return organizationMapper.findById(id);
 	}
+
 	
 //	@PostMapping("/")
 //	public int insertOrganization(@RequestParam(value = "id")Long id, @RequestParam(value = "name")String name) throws Exception{
